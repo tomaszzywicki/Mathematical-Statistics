aec0f9eb38555eb8f3e94d3156f816236a95ee9c
tomaszzywicki <tomasz.zywicki03@gmail.com>
2024-04-10 09:51:37 +0200

    add 1.3

--- Lab01/lab1.R
+++ Lab01/lab1.R
@@ -27,41 +27,41 @@
 ?curve
 # curve can plot also an expression in the variable x
 
-curve(dnorm(x, 0, 1), xlim = c(-5, 5), ylim = c(0, 0.5), col = 2, lwd = 6, lty = 2)
-curve(dnorm(x, 1, 1), xlim = c(-5, 5), ylim = c(0, 0.5), col = 2, lwd = 6, lty = 2)
-curve(dnorm(x, 2, 1), xlim = c(-5, 5), ylim = c(0, 0.5), col = 2, lwd = 6, lty = 2)
+curve(dnorm(x, 0, 1), xlim = c(-5, 5), ylim = c(0, 0.5), col = 2, lwd = 6, lty = 2, yaxs = 'i')
+curve(dnorm(x, 1, 1), xlim = c(-5, 5), ylim = c(0, 0.5), col = 2, lwd = 6, lty = 2, yaxs = 'i')
+curve(dnorm(x, 2, 1), xlim = c(-5, 5), ylim = c(0, 0.5), col = 2, lwd = 6, lty = 2, yaxs = 'i')
 
 # dystrybuanta
 
-curve(pnorm(x, 0, 1), xlim = c(-5, 5), col = 2, lwd = 6, lty = 2)
-curve(pnorm(x, 1, 1), xlim = c(-5, 5), col = 2, lwd = 6, lty = 2)
-curve(pnorm(x, 2, 1), xlim = c(-5, 5), col = 2, lwd = 6, lty = 2)
+curve(pnorm(x, 0, 1), xlim = c(-5, 5), col = 2, lwd = 6, lty = 2, yaxs = 'i')
+curve(pnorm(x, 1, 1), xlim = c(-5, 5), col = 2, lwd = 6, lty = 2, yaxs = 'i')
+curve(pnorm(x, 2, 1), xlim = c(-5, 5), col = 2, lwd = 6, lty = 2, yaxs = 'i')
 
 # funkcja przeżycia 
 
-curve(1 - pnorm(x, 0, 1), xlim = c(-5, 5), col = 2, lwd = 6, lty = 2)
-curve(1 - pnorm(x, 1, 1), xlim = c(-5, 5), col = 2, lwd = 6, lty = 2)
-curve(1 - pnorm(x, 2, 1), xlim = c(-5, 5), col = 2, lwd = 6, lty = 2)
+curve(1 - pnorm(x, 0, 1), xlim = c(-5, 5), col = 2, lwd = 6, lty = 2, yaxs = 'i')
+curve(1 - pnorm(x, 1, 1), xlim = c(-5, 5), col = 2, lwd = 6, lty = 2, yaxs = 'i')
+curve(1 - pnorm(x, 2, 1), xlim = c(-5, 5), col = 2, lwd = 6, lty = 2, yaxs = 'i')
 
 # b) N(0, 1), N(0, 0.5), N(0, 2)
 
 # gęstość
 
-curve(dnorm(x, 0, 1), xlim = c(-5, 5), ylim = c(0, 1), col = 2, lwd = 6, lty = 2)
-curve(dnorm(x, 0, 0.5), xlim = c(-5, 5), ylim = c(0, 1), col = 2, lwd = 6, lty = 2)
-curve(dnorm(x, 0, 2), xlim = c(-5, 5), ylim = c(0, 1), col = 2, lwd = 6, lty = 2)
+curve(dnorm(x, 0, 1), xlim = c(-5, 5), ylim = c(0, 1), col = 2, lwd = 6, lty = 2, yaxs = 'i')
+curve(dnorm(x, 0, 0.5), xlim = c(-5, 5), ylim = c(0, 1), col = 2, lwd = 6, lty = 2, yaxs = 'i')
+curve(dnorm(x, 0, 2), xlim = c(-5, 5), ylim = c(0, 1), col = 2, lwd = 6, lty = 2, yaxs = 'i')
 
 # dystrybuanta
 
-curve(pnorm(x, 0, 1), xlim = c(-5, 5), col = 2, lwd = 6, lty = 2)
-curve(pnorm(x, 0, 0.5), xlim = c(-5, 5), col = 2, lwd = 6, lty = 2)
-curve(pnorm(x, 0, 2), xlim = c(-5, 5), col = 2, lwd = 6, lty = 2)
+curve(pnorm(x, 0, 1), xlim = c(-5, 5), col = 2, lwd = 6, lty = 2, yaxs = 'i')
+curve(pnorm(x, 0, 0.5), xlim = c(-5, 5), col = 2, lwd = 6, lty = 2, yaxs = 'i')
+curve(pnorm(x, 0, 2), xlim = c(-5, 5), col = 2, lwd = 6, lty = 2, yaxs = 'i')
 
 # funkcja przeżycia
 
-curve(1 - pnorm(x, 0, 1), xlim = c(-5, 5), col = 2, lwd = 6, lty = 2)
-curve(1 - pnorm(x, 0, 0.5), xlim = c(-5, 5), col = 2, lwd = 6, lty = 2)
-curve(1 - pnorm(x, 0, 2), xlim = c(-5, 5), col = 2, lwd = 6, lty = 2)
+curve(1 - pnorm(x, 0, 1), xlim = c(-5, 5), col = 2, lwd = 6, lty = 2, yaxs = 'i')
+curve(1 - pnorm(x, 0, 0.5), xlim = c(-5, 5), col = 2, lwd = 6, lty = 2, yaxs = 'i')
+curve(1 - pnorm(x, 0, 2), xlim = c(-5, 5), col = 2, lwd = 6, lty = 2, yaxs = 'i')
 
 # Zadanie 2 ---------------------------------------------------------------
 
@@ -75,22 +75,32 @@ curve(dnorm(x, 0, 1), xlim = c(-4, 4), ylim = c(0,0.5), col = 2, lwd = 6, lty = 
 abline(v = c(-1, 1), col = "orange", lwd = 4, lty = 2)
 abline(v = c(-2, 2), col = "purple", lwd = 4, lty = 2)
 abline(v = c(-3, 3), col = "blue", lwd = 4, lty = 2)
-legend("topright", legend = c("1 sigma", "2 sigma", "3 sigma"), col = c("blue", "green", "red"), lty = 2)
-
-
 
 # Zadanie 3 ---------------------------------------------------------------
 
+mean_height = 173
+mean_sd = 6
 # a)
+prob = pnorm(179, mean_height, mean_sd)
+prob
+# 0.8413447
 
-
- 
 # b)
- 
+prob2 = pnorm(180, mean_height, mean_sd) - pnorm(167, mean_height, mean_sd)
+prob2
+# 0.7196722
+
 # c)
- 
+prob3 = 1 - pnorm(181, mean_height, mean_sd)
+prob3
+
+# 0.09121122
+
 # d)
- 
+height = qnorm(0.6, mean_height, mean_sd)
+height
+
+# 174.5201
 
 # Zadanie 4 ---------------------------------------------------------------
 
